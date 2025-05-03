f466b326a55d939c99aa908512e5e81aa650ec57
Joshh742 <revildobusdan@gmail.com>
2025-05-04 03:00:10 +0800

    Menambahkan endpoint untuk userd dan items

--- app/user/user.service.js
+++ app/user/user.service.js
@@ -1,6 +1,7 @@
-const { findAll, findById } = require("./user.repository");
+const { findAll, findById, create } = require("./user.repository");
 
 const getAllUsers = async () => await findAll();
 const getUserById = async (id) => await findById(id);
+const createUser = async (data) => await create(data);
 
-module.exports = { getAllUsers, getUserById };
+module.exports = { getAllUsers, getUserById, createUser };
